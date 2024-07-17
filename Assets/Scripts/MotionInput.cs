7551d48eecf9cac4fc0db69b29e42050d38eff26
minecart <ben.frankson@icloud.com>
2024-07-16 23:08:29 -0400

    Added test requirement arrays for half circles, dps, and double quarter circles
    Began testing time restraints for inputs

--- Assets/Scripts/MotionInput.cs
+++ Assets/Scripts/MotionInput.cs
@@ -17,30 +17,31 @@ public enum Direction
 public class MotionInput : MonoBehaviour
 {
     public static readonly int maxInputLength = 20;
-    public int currentFrame = 0;
+    public static readonly int maxBufferLength = 15;
+    public int currentFrame = 1;
 
     // public GameManager gameManager; // should be a script with int values currentFrame and effectiveCurrFrame
 
-    public bool testForMotionInput(TestDirection[] requirements, List<UserDirection> inputs, int indexFromEnd, int validSkips)
+    public bool testForMotionInput(TestDirection[] requirements, List<UserDirection> inputs, int inputIndex, int validSkips)
     {
-        if (indexFromEnd > inputs.Count)
+        if (inputIndex > inputs.Count)
         {
             return false;
         }
 
-        int start = indexFromEnd;
+        int startIndex = inputIndex;
         int skips = 0;
 
         for (int i = 1; i <= requirements.Length; i++)
         {
-            if (indexFromEnd > inputs.Count)
+            if (inputIndex > inputs.Count) // add a limiter to prevent iterating over every value in inputs
             {
                 return false;
             }
 
-            if (inputs[^indexFromEnd].direction == requirements[^i].direction)
+            if (inputs[^inputIndex].direction == requirements[^i].direction)
             {
-                indexFromEnd++;
+                inputIndex++;
             }
             else if (requirements[^i].skippable)
             {
@@ -57,8 +58,12 @@ public class MotionInput : MonoBehaviour
             }
         }
 
-        if (currentFrame - inputs[^start].startFrame > maxInputLength)
+        //print(inputs[^startIndex].startFrame);
+
+        if (/*inputs[^startIndex].startFrame - inputs[^(inputIndex - 1)].startFrame > maxInputLength*/
+            currentFrame - inputs[^startIndex].startFrame > maxBufferLength)
         {
+            print(currentFrame);
             return false;
         }
 
@@ -85,13 +90,100 @@ public class MotionInput : MonoBehaviour
             new TestDirection(Direction.WEST, false)
         };
 
-        List<UserDirection> inputs = new List<UserDirection>() {
-            new UserDirection(Direction.SOUTH, 3),
-            new UserDirection(Direction.SOUTHEAST, 4),
-            new UserDirection(Direction.EAST, 5)
+        TestDirection[] dpfor_requirements = new TestDirection[] {
+            new TestDirection(Direction.EAST, false),
+            new TestDirection(Direction.SOUTHEAST, true),
+            new TestDirection(Direction.SOUTH, false),
+            new TestDirection(Direction.SOUTHEAST, false),
+            new TestDirection(Direction.EAST, true)
         };
 
-        if (testForMotionInput(qfor_requirements, inputs, 1, 0))
+        TestDirection[] dpback_requirements = new TestDirection[] {
+            new TestDirection(Direction.WEST, false),
+            new TestDirection(Direction.SOUTHWEST, true),
+            new TestDirection(Direction.SOUTH, false),
+            new TestDirection(Direction.SOUTHWEST, false),
+            new TestDirection(Direction.WEST, true)
+        };
+
+        TestDirection[] hfor_requirements = new TestDirection[] {
+            new TestDirection(Direction.WEST, false),
+            new TestDirection(Direction.SOUTHWEST, true),
+            new TestDirection(Direction.SOUTH, true),
+            new TestDirection(Direction.SOUTHEAST, true),
+            new TestDirection(Direction.EAST, false)
+        };
+
+        TestDirection[] hback_requirements = new TestDirection[] {
+            new TestDirection(Direction.EAST, false),
+            new TestDirection(Direction.SOUTHEAST, true),
+            new TestDirection(Direction.SOUTH, true),
+            new TestDirection(Direction.SOUTHWEST, true),
+            new TestDirection(Direction.WEST, false)
+        };
+
+        TestDirection[] doubleqfor_requirements = new TestDirection[] {
+            new TestDirection(Direction.SOUTH, false),
+            new TestDirection(Direction.SOUTHEAST, true),
+            new TestDirection(Direction.EAST, false),
+            new TestDirection(Direction.SOUTH, true),
+            new TestDirection(Direction.SOUTHEAST, true),
+            new TestDirection(Direction.EAST, true)
+        };
+
+        TestDirection[] doubleqback_requirements = new TestDirection[] {
+            new TestDirection(Direction.SOUTH, false),
+            new TestDirection(Direction.SOUTHWEST, true),
+            new TestDirection(Direction.WEST, false),
+            new TestDirection(Direction.SOUTH, true),
+            new TestDirection(Direction.SOUTHWEST, true),
+            new TestDirection(Direction.WEST, true)
+        };
+
+        /*
+        TestDirection[] hplusfor_requirements = new TestDirection[] {
+            new TestDirection(Direction.EAST, false),
+            new TestDirection(Direction.SOUTHEAST, true),
+            new TestDirection(Direction.SOUTH, true),
+            new TestDirection(Direction.SOUTHWEST, true),
+            new TestDirection(Direction.WEST, false),
+            new TestDirection(Direction.EAST, false)
+        };
+        */
+
+        List<UserDirection> inputs = new List<UserDirection>() {
+            new UserDirection(Direction.SOUTH, 1),
+            new UserDirection(Direction.WEST, 1),
+            new UserDirection(Direction.SOUTH, 1),
+            new UserDirection(Direction.SOUTHWEST, 1),
+            new UserDirection(Direction.WEST, 2)
+        };
+
+        if (testForMotionInput(doubleqfor_requirements, inputs, 1, 2))
+        {
+            print("doubleqfor");
+        }
+        else if (testForMotionInput(doubleqback_requirements, inputs, 1, 2))
+        {
+            print("doubleqback");
+        }
+        else if (testForMotionInput(hfor_requirements, inputs, 1, 1))
+        {
+            print("hfor");
+        }
+        else if (testForMotionInput(hback_requirements, inputs, 1, 1))
+        {
+            print("hback");
+        }
+        else if (testForMotionInput(dpfor_requirements, inputs, 1, 2))
+        {
+            print("dpfor");
+        }
+        else if (testForMotionInput(dpback_requirements, inputs, 1, 2))
+        {
+            print("dpback");
+        }
+        else if (testForMotionInput(qfor_requirements, inputs, 1, 0))
         {
             print("qfor");
         }
@@ -99,7 +191,11 @@ public class MotionInput : MonoBehaviour
         {
             print("qback");
         }
-
+        else
+        {
+            print("none");
+        }
+        
         currentFrame++;
     }
 }
